bba0070e9e7140bda11fa0292ce3cbb8e6f5251b
martinwamb <martinwamb@gmail.com>
2025-12-08 18:55:42 +0300

    updating image size upload

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 
